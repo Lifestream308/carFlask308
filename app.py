81be8d6da30af3603a6100fb410899f540ac8a20
Lifestream308 <99936729+Lifestream308@users.noreply.github.com>
2022-08-25 11:06:47 -0700

    Add files via upload
    small change, added some comments/directions to file app.py about uploading to Heroku

--- app.py
+++ app.py
@@ -12,3 +12,6 @@ if __name__ == '__main__':
 
     # to setup venv I entered 
     # py -m venv venv          second venv is the name of the virtual environment I believe
+
+
+    # When time to initiate database for heroku, it is: heroku run python --app carflask308
