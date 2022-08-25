3115c8655ba55bb40a94479b50ff33a9508a1a1f
Lifestream308 <99936729+Lifestream308@users.noreply.github.com>
2022-08-25 11:04:58 -0700

    Add files via upload
    Changed database URI in the __init__ file because Heroku did maintenance and changed the URI/location of the database

--- carflask/__init__.py
+++ carflask/__init__.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # Below is elephant sql database 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jowfndzs:s_fKMzxmDIg6D1gpNd2s_fLNlahUzEh3@heffalump.db.elephantsql.com/jowfndzs'
-# Below is Heroku database 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bwefhrbxvfhmuc:afab3e90282c5ee93ae50e6d96041c61ace9b0e3d10bdd3b26187f4bbe7fa65b@ec2-34-227-135-211.compute-1.amazonaws.com:5432/ddvqeel8ank3q5'
+# Below is Heroku database  ....    update: Heroku did "maintenance" looks like database was changed/moved
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bwefhrbxvfhmuc:afab3e90282c5ee93ae50e6d96041c61ace9b0e3d10bdd3b26187f4bbe7fa65b@ec2-34-227-135-211.compute-1.amazonaws.com:5432/ddvqeel8ank3q5'
+# This is the name of the database after Heroku's maintenance
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hdjumfrahltkmf:34241e5f4ddb3c9e1b9c3f3087679700717b313f6b6be821ba37caf86cbf1a2f@ec2-18-208-55-135.compute-1.amazonaws.com:5432/du4jictpj9ia8'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 ma = Marshmallow(app)
